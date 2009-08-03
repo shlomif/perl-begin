084364ca073f82e15d23937f3a6d0166ac400e37
shlomif <shlomif@13c2ddb8-3aa7-4284-800b-8db07c650eef>
2009-08-03 20:34:28 +0000

    Corrected the lect_source to the new location on the disk.

--- lib/tutorials/perl-for-newbies/update.sh
+++ lib/tutorials/perl-for-newbies/update.sh
@@ -4,7 +4,7 @@ for I in 1 2 3 4 ; do
     (
         echo "Processing Part #$I"
         cd lect"$I"-all-in-one
-        lect_source="$HOME/Docs/lecture/Perl/Newbies/lecture$I/"
+        lect_source="$HOME/Docs/homepage/homepage/trunk/lib/presentations/qp/perl-for-newbies/$I/"
         (cd $lect_source && \
             perl "$HOME"/progs/quadpres/trunk/temporary-scripts/render-all-in-one-page.pl 
         )
