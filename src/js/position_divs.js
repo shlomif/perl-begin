94b6803ea1c9fb941a56c4e78914e6a31bf4f003
alanhaggai <alanhaggai@13c2ddb8-3aa7-4284-800b-8db07c650eef>
2009-09-02 17:13:46 +0000

    Merged branch site-redesign-using-compass

---
+++ src/js/position_divs.js
@@ -0,0 +1,20 @@
+function position_divs() {
+    var width = document.body.clientWidth - 780;
+    document.getElementById("js-site-name").style.left   = width / 2 + 20 + "px";
+    document.getElementById("js-site-slogan").style.left = width / 2 + 20 + "px";
+    document.getElementById("js-nav-global").style.right = width / 2 - 20 + "px";
+    if ( document.getElementById("right-sidebar") == null ) {
+        document.getElementById("content").style.width = document.body.clientWidth - 250 + "px";
+    }
+    else {
+        document.getElementById("content").style.width = document.body.clientWidth - 410 + "px";
+    }
+}
+
+$(document).ready( function() {
+        position_divs();
+} );
+
+$(window).resize( function() {
+        position_divs();
+} );
