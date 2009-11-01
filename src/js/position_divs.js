2c2a7763abef87e6fc823cc91dc25dca6c82d50d
shlomif <shlomif@13c2ddb8-3aa7-4284-800b-8db07c650eef>
2009-11-01 13:13:13 +0000

    [position_divs.js] Extracted a variable for the document.body.clientWidth.

--- src/js/position_divs.js
+++ src/js/position_divs.js
@@ -1,14 +1,15 @@
 function position_divs() {
-    var width = document.body.clientWidth - 780;
+    var client_w = document.body.clientWidth ;
+    var width = client_w - 780;
     document.getElementById("js-site-name").style.left   = width / 2 + 20 + "px";
     document.getElementById("js-site-slogan").style.left = width / 2 + 20 + "px";
     // Commenting out because it no longer exists.
     // document.getElementById("js-nav-global").style.right = width / 2 - 20 + "px";
     if ( document.getElementById("right-sidebar") == null ) {
-        document.getElementById("content").style.width = document.body.clientWidth - 250 + "px";
+        document.getElementById("content").style.width = client_w - 250 + "px";
     }
     else {
-        document.getElementById("content").style.width = document.body.clientWidth - 410 + "px";
+        document.getElementById("content").style.width = client_w - 410 + "px";
     }
 }
 
