ded2d1e517eea00fda0dac34c0c2b3b3c8c0eb87
shlomif <shlomif@13c2ddb8-3aa7-4284-800b-8db07c650eef>
2009-11-01 15:55:35 +0000

    Merged the CSS tweaks from the css-tweaking-for-the-about-links branch.
    Made sure the about-links and breadcrumbs trails are semantic with more
    well-defined classes.

--- src/js/position_divs.js
+++ src/js/position_divs.js
@@ -1,20 +1,20 @@
 function position_divs() {
-    var width = document.body.clientWidth - 780;
-    document.getElementById("js-site-name").style.left   = width / 2 + 20 + "px";
-    document.getElementById("js-site-slogan").style.left = width / 2 + 20 + "px";
-    document.getElementById("js-nav-global").style.right = width / 2 - 20 + "px";
-    if ( document.getElementById("right-sidebar") == null ) {
-        document.getElementById("content").style.width = document.body.clientWidth - 250 + "px";
-    }
-    else {
-        document.getElementById("content").style.width = document.body.clientWidth - 410 + "px";
-    }
+    var client_w = document.body.clientWidth ;
+    var width = client_w - 780;
+    var js_site_w_str = width / 2 + 20 + "px";
+
+    var set_left = function (my_id) { $("#"+my_id).css("left", js_site_w_str);};
+
+    set_left("js-site-name");
+    set_left("js-site-slogan");
+
+    // Commenting out because it no longer exists.
+    // set_left("js-nav-global");
+
+    $("#content").css("width",
+        client_w - ($("#right-sidebar") ? 410 : 250) + "px"
+    );
 }
 
-$(document).ready( function() {
-        position_divs();
-} );
-
-$(window).resize( function() {
-        position_divs();
-} );
+$(document).ready(position_divs);
+$(window).resize(position_divs);
