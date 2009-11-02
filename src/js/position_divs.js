bdb79ab324fec139553480e23c17df94999b79c9
shlomif <shlomif@13c2ddb8-3aa7-4284-800b-8db07c650eef>
2009-11-02 20:24:02 +0000

    [Perl-Begin] Fixed the CSS/JS to have a common element.

--- src/js/position_divs.js
+++ src/js/position_divs.js
@@ -1,18 +1,8 @@
 function position_divs() {
     var client_w = document.body.clientWidth ;
-    var width = client_w - 780;
-    var js_site_w_str = width / 2 + 20 + "px";
-
-    var set_left = function (my_id) { $("#"+my_id).css("left", js_site_w_str);};
-
-    set_left("js-site-name");
-    set_left("js-site-slogan");
-
-    // Commenting out because it no longer exists.
-    // set_left("js-nav-global");
 
     $("#content").css("width",
-        client_w - ($("#right-sidebar") ? 410 : 250) + "px"
+        client_w - 20 - ($("#right-sidebar") ? 410 : 250) + "px"
     );
 }
 
