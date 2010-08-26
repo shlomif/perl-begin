c592f1c8aa802b17ed29056f188247de5a9558ac
shlomif <shlomif@13c2ddb8-3aa7-4284-800b-8db07c650eef>
2010-08-26 21:42:39 +0000

    Fixed the CSS.
    The UL/OL were broken due to being explicitly set in blueprint.

--- config.rb
+++ config.rb
@@ -3,8 +3,8 @@ project_type = :stand_alone
 css_dir = "src"
 sass_dir = "lib/sass"
 images_dir = "src/images"
-output_style = :compressed
-# output_style = :expanded
+# output_style = :compressed
+output_style = :expanded
 # To enable relative image paths using the images_url() function:
 # http_images_path = :relative
 http_images_path = "images"
