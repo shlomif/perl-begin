913b7e90a821a6401457934cdd185e5a292a7e31
Shlomi Fish <shlomif@shlomifish.org>
2012-12-23 21:27:48 +0200

    More spellchecking.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -2,6 +2,7 @@
 find dest -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
     grep -vP '^dest/tutorials/modern-perl/' |
+    grep -vP '^dest/tutorials/impatient-perl/iperl\.html' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |
