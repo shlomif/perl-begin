5a2a979a73c00052fa3db4705858ffc26afcc9f2
Shlomi Fish <shlomif@shlomifish.org>
2012-12-24 00:43:54 +0200

    Go the tests to pass again.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -2,6 +2,7 @@
 find dest -name '*.html' -or -name '*.xhtml' |
     ( LC_ALL=C sort  ) |
     grep -vP '^dest/tutorials/modern-perl/' |
+    grep -vP '^dest/tutorials/hyperpolyglot/' |
     grep -vP '^dest/tutorials/impatient-perl/iperl\.html' |
     grep -vP '^dest/topics/cpan/life-with-cpan/' |
     grep -vP '^dest/humour/' |
