3103b6383ee291038842f071c33f451f77092a06
Shlomi Fish <shlomif@shlomifish.org>
2012-12-23 22:41:29 +0200

    More spell-checking.

--- bin/spell-checker-iface.sh
+++ bin/spell-checker-iface.sh
@@ -4,6 +4,7 @@ find dest -name '*.html' -or -name '*.xhtml' |
     grep -vP '^dest/tutorials/modern-perl/' |
     grep -vP '^dest/tutorials/impatient-perl/iperl\.html' |
     grep -vP '^dest/topics/cpan/life-with-cpan/' |
+    grep -vP '^dest/humour/' |
     xargs perl bin/html-check-spelling-xmlp.pl |
     grep ':'
     # perl -lne 'print if /MathVentures\/3d.*\.xhtml/' |
