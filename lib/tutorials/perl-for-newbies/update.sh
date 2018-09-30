b8eafaa05631b08c7f6062d470d00113dd989ec5
Shlomi Fish <shlomif@shlomifish.org>
2018-09-30 08:46:01 +0300

    update to new syntax

--- lib/tutorials/perl-for-newbies/update.sh
+++ lib/tutorials/perl-for-newbies/update.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-for I in 1 2 3 4 5 ; do
+for part_idx in 1 2 3 4 5
+do
     (
-        echo "Processing Part #$I"
-        cd lect"$I"-all-in-one
-        lect_source="$HOME/Docs/homepage/homepage/trunk/lib/presentations/qp/perl-for-newbies/$I/"
-        (cd $lect_source && \
-            perl -I "$(pwd)" "$HOME"/progs/quadpres/trunk/temporary-scripts/render-all-in-one-page.pl
-        )
+        echo "Processing Part #$part_idx"
+        cd lect"$part_idx"-all-in-one
+        lect_source="$HOME/Docs/homepage/homepage/trunk/lib/presentations/qp/perl-for-newbies/$part_idx/"
+        if false
+        then
+            (cd $lect_source && \
+                quadp render_all_in_one_page --output-dir all-in-one
+            )
+        fi
         cp -af "$lect_source"/all-in-one/* .
     )
 done
