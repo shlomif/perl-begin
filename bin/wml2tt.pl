823c8352002c10cef9c8fcfaac514efdc520ba02
Shlomi Fish <shlomif@shlomifish.org>
2019-02-04 16:18:41 +0200

    Convert a page to tt2

--- bin/wml2tt.pl
+++ bin/wml2tt.pl
@@ -11,3 +11,9 @@ s#<(cpan_self_dist|cpan_self_mod|pdoc_f) ([^>]+)/>#
 s#<pdoc_f f="(\w+)">#[%- WRAPPER pdoc_f f = "$1" -%]#g;
 s#</pdoc_f>#[%- END -%]#g;
 s/\$\(ROOT\)\//[% base_path %]/g;
+
+s{\A#include "template5.wml"\n+<latemp_subject ("[^"]*") />\n+}{[%- SET title = $1 -%]
+[%- PROCESS "blocks.tt2" -%]
+[%- WRAPPER wrap_html -%]\n\n};
+
+$_ .= "\n[% END %]\n";
