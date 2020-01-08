c15b430004fd592a5617a22c5e5408a4f4602e6e
Shlomi Fish <shlomif@shlomifish.org>
2020-01-08 14:50:04 +0200

    package.json

--- bin/install-npm-deps.sh
+++ bin/install-npm-deps.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # install-npm-deps.sh
 # Copyright (C) 2018 shlomif <shlomif@cpan.org>
 #
 # Distributed under terms of the MIT license.
 #
-npm install \
-    html-minifier \
-    jquery \
-    prettier \
-    sass \
+set -e -x
+npm install
+exit
+
+#     html-minifier \
+#     jquery \
+#     prettier \
+#     sass \
