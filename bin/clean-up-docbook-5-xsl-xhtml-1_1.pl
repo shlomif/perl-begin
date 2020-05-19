2d6781f244a5f8ff1653c9382feb329819f1c588
Shlomi Fish <shlomif@shlomifish.org>
2020-05-19 17:01:15 +0300

    add syntax highlighting to "bad-elements/"

--- bin/clean-up-docbook-5-xsl-xhtml-1_1.pl
+++ bin/clean-up-docbook-5-xsl-xhtml-1_1.pl
@@ -5,9 +5,12 @@ use warnings;
 use autodie;
 
 use Path::Tiny qw/ path /;
-use XML::LibXML;
-use XML::LibXML::XPathContext;
-use Getopt::Long;
+
+# use XML::LibXML;
+# use XML::LibXML::XPathContext;
+use Getopt::Long qw/ GetOptions /;
+use HTML::Entities qw/ decode_entities /;
+use Text::VimColor;
 
 my $out_fn;
 
@@ -34,4 +37,27 @@ $s =~ s{<(/?)h(\d)}{"<".$1."h".($2+2)}ge;
 
 $s =~ s/[ \t]+$//gms;
 
+sub _syntax_highlight_code
+{
+    my ($code) = @_;
+
+    $code = decode_entities($code);
+
+    my $tvc = Text::VimColor->new(
+        string   => \$code,
+        filetype => "perl",
+    );
+
+    my $CANARY = "glamoutttttttttTTTTTTTTTTTTTTTTTTtTTTTTTTTTT";
+    return scalar( $tvc->html() =~ s{(class=")syn}{$1}gr );
+}
+
+my $CLASS1 = 'perl';
+my $CLASS2 = 'programlisting';
+
+$s =~
+    s%<pre class="(?:(?:$CLASS1\s+$CLASS2)|(?:$CLASS2\s+$CLASS1))">(.*?)</pre>%
+    my $code = $1;
+    qq#<pre class="code $CLASS1 $CLASS2"># . _syntax_highlight_code($code) . qq#</pre>#
+%egms;
 path($out_fn)->spew_utf8($s);
