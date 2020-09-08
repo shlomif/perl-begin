5ad60199d94d5312048cfc8cf7be42f6e6a45d2e
Shlomi Fish <shlomif@shlomifish.org>
2020-09-08 14:54:10 +0300

    Update appveyor / Travis-CI.
    Based on https://github.com/shlomif/perl6-CI-Gen and
    https://github.com/shlomif/ci-gen-framework .
    
    See:
    
    * https://en.wikipedia.org/wiki/Travis_CI
    
    * https://en.wikipedia.org/wiki/Continuous_integration .

--- .travis.bash
+++ .travis.bash
@@ -37,7 +37,7 @@ then
     eval "$(perl -I ~/perl_modules/lib/perl5 -Mlocal::lib=$HOME/perl_modules)"
     cpanm App::Deps::Verify App::XML::DocBook::Builder Pod::Xhtml
     export CPATH="${CPATH}:/usr/include/tidy"
-    cpanm -vvvvvvvvvv --no-interactive HTML::T5
+    cpanm HTML::T5
     # For wml
     cpanm --notest Bit::Vector Carp::Always Class::XSAccessor GD Getopt::Long IO::All Image::Size List::MoreUtils Path::Tiny Term::ReadKey
     # For quadp
