9a0745211e84e2af7105ace83aaef85d129382db
Shlomi Fish <shlomif@shlomifish.org>
2021-12-20 17:48:31 +0200

    promote the intro to start of text [SEO/UX]

--- bin/pre-clean-up-docbook-5-xsl.pl
+++ bin/pre-clean-up-docbook-5-xsl.pl
@@ -8,6 +8,29 @@ use strict;
 use warnings;
 use 5.014;
 use autodie;
+use XML::LibXML;
+use XML::LibXML::XPathContext;
+my $s   = $_;
+my $xml = XML::LibXML->load_xml( string => $s );
+my $xpc = XML::LibXML::XPathContext->new($xml);
+$xpc->registerNs( 'xhtml', 'http://www.w3.org/1999/xhtml' );
+my @intro = $xpc->findnodes(
+    q#//xhtml:section[@class = 'section' and descendant::*[@id='intro']]#);
+die if @intro != 1;
+my @parent = $xpc->findnodes(q#//xhtml:section[@class='article']#);
+my @heads  = $xpc->findnodes(
+q#//xhtml:div[@class='titlepage']/descendant::*[starts-with(local-name(), 'h')]#
+);
+die if @parent != 1;
+die if @intro != 1;
+die if @heads == 0;
+my $i = $intro[0];
+$i->parentNode->removeChild($i);
+
+my $p = $parent[0];
+$p->insertBefore( $i,        $p->firstChild );
+$p->insertBefore( $heads[0], $p->firstChild );
+$_ = $xml->toString();
 s/[ \t]+$//gms;
 s{\Q xmlns="http://docbook.org/ns/docbook"\E}{}g;
 s{(</?h)([0-9]+)}{$1.($2-1)}eg;
