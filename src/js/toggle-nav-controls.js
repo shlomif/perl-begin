68260a11afdfcb19d892da8e5c9b4d7ab8373216
Shlomi Fish <shlomif@shlomifish.org>
2025-01-07 15:44:49 +0200

    persist state using localStorage
    Thanks to MDN and jQuery

--- src/js/toggle-nav-controls.js
+++ src/js/toggle-nav-controls.js
@@ -4,18 +4,30 @@ function _toggle_nav_control__is_on() {
     return (!(elem.hasClass("off")));
 }
 
+const _toggle_nav_control__localStorage_key = "toggle_nav_state";
+
 function toggle_nav_control() {
     const elem = $("#toggle_nav_control");
 
+    let newval;
     if (!_toggle_nav_control__is_on()) {
         elem.text("Hide Navigation Controls");
+        newval = "on";
     }
     else {
         elem.text("Show Navigation Controls");
+        newval = "off";
     }
+    localStorage.setItem(_toggle_nav_control__localStorage_key, newval);
     $(".page-nav-bar, .bread").toggleClass("vis");
     elem.toggleClass("off");
     elem.toggleClass("on");
 }
 
+function _toggle_nav_onstart () {
+    if (localStorage.getItem(_toggle_nav_control__localStorage_key) === "on") {
+        return toggle_nav_control();
+    }
+}
 
+$(_toggle_nav_onstart);
